f2f2607691ca7913161442b99ddf67c435df2c4a
binas <binas@BINJEN>
2019-08-05 19:54:18 +0200

    Display redmine tickets

---
+++ Estimator/ViewModel/EstimatorViewModel.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Estimator.ViewModel
+{
+    class EstimatorViewModel : ObservableObject
+    {
+        private ICommand getIssues;
+        public ICommand GetIssues
+        {
+            get
+            {
+                if (getIssues == null)
+                {
+                    getIssues = new RelayCommand(p => new GetIssues());
+                }
+                return getIssues;
+            }
+        }
+    }
+}
