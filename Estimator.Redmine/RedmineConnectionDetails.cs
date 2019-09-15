ab48052445b0ef607434709bd9cb7d7ccdbea4f8
binas <binas@BINJEN>
2019-09-15 17:42:19 +0200

    Renaming - RedmineConnectionDetails class to RedmineConnectionProvider and implementing

--- Estimator.Redmine/RedmineConnectionDetails.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Estimator.Redmine
-{ 
-   public class RedmineConnectionDetails
-    {
-        public string Host = "http://127.0.0.1/redmine/";
-        public string Api = "e698087c45046c4e5b45e7b666fc3aa51d94f37f";
-    }
-}
---
+++ Estimator.Redmine/RedmineConnectionProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+
+namespace Estimator.Redmine
+{ 
+   public class RedmineConnectionProvider
+    {
+        public RedmineConnectionProvider()
+        {
+            Host = ConfigurationManager.AppSettings["Host"];
+            Api = ConfigurationManager.AppSettings["Api"];
+        }
+        public static string Host { get; set; }
+        public static string Api { get; set; }
+    }
+}
