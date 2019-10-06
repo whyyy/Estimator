820c89c4706608842d4bc1e52d33b3bee412b535
binas <binas@BINJEN>
2019-10-06 20:09:38 +0200

    Removing unused constructors

--- Estimator.Redmine/Models/Ticket.cs
+++ Estimator.Redmine/Models/Ticket.cs
@@ -6,11 +6,6 @@ namespace Estimator.Redmine.Model
 {
     public class Ticket 
     {
-        public Ticket(string statusName, int statusId)
-        {
-            StatusName = statusName;
-            StatusId = statusId;
-        }
         public Ticket(int id, string subject, DateTime? startDate,
             DateTime? endDate, DateTime? uatStartDate, DateTime? uatEndDate,
             int status, string statusName, IList<IssueCustomField> customFields)
@@ -24,23 +19,8 @@ namespace Estimator.Redmine.Model
             StatusId = status;
             StatusName = statusName;
             CustomFields = customFields;
-
         }
-        public Ticket(int id, string subject, DateTime? startDate,
-           DateTime? endDate, DateTime? uatStartDate, DateTime? uatEndDate,
-           int status, string statusName, IList<IssueCustomField> customFields, string milestoneId)
-        {
-            Id = id;
-            Subject = subject;
-            StartDate = startDate;
-            EndDate = endDate;
-            UatStartDate = uatStartDate;
-            UatEndDate = uatEndDate;
-            StatusId = status;
-            StatusName = statusName;
-            CustomFields = customFields;
-            MilestoneId = milestoneId;
-        }
+       
         public int Id { get; set; }
         public string Subject { get; set; }
         public DateTime? StartDate { get; set; }
