f3f57809defb36b89812bb8aa0ea3c4cb21fc3c3
binas <binas@BINJEN>
2019-10-06 20:13:25 +0200

    Little cleanup

--- Estimator.Data/Model/DataProvider.cs
+++ Estimator.Data/Model/DataProvider.cs
@@ -1,6 +1,5 @@
 ﻿using Estimator.Testrail.Model;
 using Estimator.Redmine.Model;
-using System;
 using System.Collections.Generic;
 
 namespace Estimator.Data.Model
@@ -8,21 +7,36 @@ namespace Estimator.Data.Model
     public class DataProvider
     {
         private RedmineDataProvider _redmineData;
+
         public DataProvider()
         {
             RedmineData = GetRedmineData();
         }
-        public RedmineDataProvider GetRedmineData()
-        {
-            _redmineData = new RedmineDataProvider();
-            return _redmineData;
-        }
 
+        public List<Ticket> Tickets { get; set; }
         public List<Status> Statuses { get; set; }
         public RedmineDataProvider RedmineData { get; set; }
         public TestrailDataProvider TestrailData { get; set; }
         public Status Status { get; set; }
         public Ticket Ticket { get; set; }
-        public TestRun Testrun { get; set; }    
+        public TestRun Testrun { get; set; }
+
+        public RedmineDataProvider GetRedmineData()
+        {
+            _redmineData = new RedmineDataProvider();
+            return _redmineData;
+        }
+        public List<Ticket> GetTickets(int selectedStatusId)
+        {
+            Tickets = new List<Ticket>();
+            foreach (Ticket ticket in RedmineData.Tickets)
+            {
+                if (ticket.StatusId.Equals(selectedStatusId))
+                {
+                    Tickets.Add(ticket);
+                }
+            }
+            return Tickets;
+        }
     }
 }
