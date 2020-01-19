e83d9d31bee3cc8748a2d35e4d01aa36491abce3
binas <binas@BINJEN>
2020-01-19 19:00:29 +0100

    Update to interface

--- Estimator.Redmine/IRedmineConnectionProvider.cs
+++ Estimator.Redmine/IRedmineConnectionProvider.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Redmine.Net.Api;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace Estimator.Redmine
 {
     public interface IRedmineConnectionProvider
     {
-        RedmineConnectionDetails GetRedmineConnectionDetails();
+        RedmineManager GetRedmineConnection();
     }
 }
